782d799b16ee884cc3474a1e1d140e3581b70194
Aaron <aaron.mcniven@outlook.com>
2016-07-05 23:17:09 +1000

    Update ConfigFile.h

--- ConfigFile.h
+++ ConfigFile.h
@@ -18,10 +18,10 @@ public:
 	bool load(const std::string &filename);
 	std::string getLocation();
 	bool valueExists(const std::string &name);
-    std::pair<std::string, bool> getValueString(const std::string &name);
-    std::pair<bool, bool> getValueBool(const std::string &name);
-    std::pair<int, bool> getValueInt(const std::string &name);
-    std::pair<float, bool> getValueFloat(const std::string &name);
+    	std::pair<std::string, bool> getValueString(const std::string &name);
+    	std::pair<bool, bool> getValueBool(const std::string &name);
+    	std::pair<int, bool> getValueInt(const std::string &name);
+    	std::pair<float, bool> getValueFloat(const std::string &name);
 	~ConfigFile();
 };
 

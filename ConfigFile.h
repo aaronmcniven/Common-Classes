ed084131ef39a427e99c3cbaa83b13e34edbdea7
Aaron <aaron.mcniven@outlook.com>
2016-07-06 17:10:18 +1000

    Update ConfigFile.h

--- ConfigFile.h
+++ ConfigFile.h
@@ -1,22 +1,25 @@
 #pragma once
 
+#include <Windows.h>
+#include <iostream>
 #include <string>
 #include <vector>
 #include <fstream>
 #include <algorithm>
+#include <map>
 
 class ConfigFile {
 private:
+	bool m_loaded;
 	std::string m_location;
-	std::vector<std::string> m_names;
-	std::vector<std::string> m_values;
-	std::ifstream file;
+	std::map<std::string, std::string> m_values;
 
 public:
 	ConfigFile();
 	ConfigFile(const std::string &filename);
 	bool load(const std::string &filename);
-	std::string getLocation();
+	bool loaded();
+	const std::string &getLocation();
 	bool valueExists(const std::string &name);
     	std::pair<std::string, bool> getValueString(const std::string &name);
     	std::pair<bool, bool> getValueBool(const std::string &name);
